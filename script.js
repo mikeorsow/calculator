1ef2fe0cd50b8eeafa7c8278f2b19776cbb09fdc
Mike Orsow <mike.orsow@gmail.com>
2022-07-31 22:26:18 -0700

    Make plus and equal buttons work

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
+const displayed = document.querySelector("#display > p");
+const numberButtons = document.querySelectorAll(".number");
+const sumButton = document.querySelector("#sum");
+const equalsButton = document.querySelector("#equals");
 
-const displayed = document.querySelector('#display > p');
-const numberButtons = document.querySelectorAll('.number');
+let currentNumber = "";
+let subTotal = "";
+let total = "";
+let currentOperator = "";
 
-const updateDisplay = (e) => {
-    return displayed.textContent = `${displayed.textContent}${e.target.textContent}`;
- }
+const displayOnScreen = (num) => {
+  document.querySelector("#display > p").textContent = num;
+};
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', (e) => updateDisplay(e))
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // start a new number if there is no operator
+    if (currentOperator == "") {
+      total = "";
+    }
+    currentNumber += e.target.textContent;
+    displayOnScreen(currentNumber);
+  });
 });
 
+sumButton.addEventListener("click", () => {
+  return (displayed.textContent = sum());
+});
 
+const sum = () => {
+  currentOperator = "plus";
+  if (currentNumber == "" && subTotal == "") {
+    subTotal = total;
+  } else {
+    subTotal = Number(currentNumber) + Number(subTotal);
+  }
+  currentNumber = "";
+  return subTotal;
+};
 
+equalsButton.addEventListener("click", () => showTotal());
 
-
-const handleNumber = () => updateDisplay()
+const showTotal = () => {
+  if (currentOperator == "") {
+    return;
+  }
+  if (currentOperator == "plus") {
+    total = sum();
+  }
+  //clear everything except Total
+  subTotal = "";
+  (currentNumber = ""), (currentOperator = "");
+  return (displayed.textContent = total);
+};
